41eae674c53702e3c9a3ff1e6c3f9a1d5352a03a
ΔΗΜΗΤΡΙΟΣ ΓΚΟΡΟΣ <100197395+Dimitrioz@users.noreply.github.com>
2022-03-11 21:34:50 +0200

    comments update
    I putted comments everywhere boiiii

--- Wittol/Wittol/main.cpp
+++ Wittol/Wittol/main.cpp
@@ -2,35 +2,35 @@
 #include<limits>
 using namespace std;
 int main() {
-	string name;
-	string country;
-	int age = 100000;
-	cout << "Please,eneder your name: ";
-	cin >> name;
-	cout << "Wellcome to Wittol," << name;
-	cout << "\nWhere are you from " << name << "?: ";
-	cin >> country;
+	string name;//the string of thename of prodagonist
+	string country;//the stgring of the counntry of prodagoniost
+	int age = 100000;//the intigure of age
+	cout << "Please,ender your name: ";//here the player has to add a name
+	cin >> name;//here the computer will read the name
+	cout << "Wellcome to Wittol," << name;//wellcome message for the player
+	cout << "\nWhere are you from " << name << "?: ";//the player must complete where is from
+	cin >> country;//the computer reads the country the player is from
 	cout << "OMG i love " << country;
-	bool validInput = false;
-	cout << "\nHow old are you?: ";
-	while (!validInput)
+	bool validInput = false;//if the age is wrong 
+	cout << "\nHow old are you?: ";//the age the player must submit 
+	while (!validInput)// the loop for the age
 	{
 		if (cin >> age)
 		{
 			validInput = true;
-			if (age >= 18 && age <= 100)
+			if (age >= 18 && age <= 100)//contition if age is bigger than 18
 			{
 				cout << "Big boy!\n";
 				validInput = true;
 			}
-			else if (age > 0 && age < 18)
+			else if (age > 0 && age < 18)//contition if age is less than 18
 			{
 				cout << "Smalle boye!\n";
 				validInput = true;
 			}
 			else
 			{
-				validInput = false;
+				validInput = false;//contition if player says wrong age
 				cout << "Liar boye! Try again: ";
 				cin.clear();
 				cin.ignore(numeric_limits<streamsize>::max(), '\n');
@@ -38,20 +38,20 @@ int main() {
 		}
 		else
 		{
-			cout << "Invalid input! Try again: ";
+			cout << "Invalid input! Try again: ";//codition if player says stupid word
 			cin.clear();
 			cin.ignore(numeric_limits<streamsize>::max(), '\n');
 		}
 	}
-	string food;
+	string food;//we asking fav food
 	string sport;
 	cout << "\nWhat is your favorite food: ";
 	cin >> food;
 	cout << "OMG,i like " << food << " a lot, kinda spicy tho";
-	cout << "\nSo,what is your favorite sport?: ";
+	cout << "\nSo,what is your favorite sport?: ";// we asking fav spor
 	cin >> sport;
 	cout << sport << " is very cool,i played a bit as well back in the day,not for long tho";
-	cout << "\nIf you could travel somewhere,where would you liek to go?: ";
+	cout << "\nIf you could travel somewhere,where would you like to go?: ";//we asking where the player likes to travel
 	return 0;
 }
 
