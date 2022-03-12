0d715be1b2fabc927bbc3303363c4423ebecac62
ΔΗΜΗΤΡΙΟΣ ΓΚΟΡΟΣ <100197395+Dimitrioz@users.noreply.github.com>
2022-03-12 23:50:58 +0200

    Mini update on countries
    i made the menu for the countries of each continent
    the menu is incomplete tho

--- wittol_cpp/Wittol_cpp/Wittol_cpp.cpp
+++ wittol_cpp/Wittol_cpp/Wittol_cpp.cpp
@@ -14,7 +14,7 @@ int main() {
 	cin >> name;
 	system("cls");
 	cout << "Wellcome to Wittol," << name;
-	cout << "\n\nWhere are you from " << name << "?: ";
+	cout << "\n\nWhere are you from, " << name << "?: ";
 	cin >> country;
 	system("cls");
 	cout << "OMG i love " << country;
@@ -57,7 +57,7 @@ int main() {
 	string food;//we asking fav food
 	string sport;
 	char choice;
-	cout << "\nWhat is your favorite food "<<name<<"?: ";
+	cout << "\nWhat is your favorite food " << name << "?: ";
 	cin >> food;
 	system("cls");
 	cout << "OMG,i like " << food << " a lot, kinda spicy tho";
@@ -65,7 +65,7 @@ int main() {
 	cin >> sport;
 	system("cls");
 	cout << sport << " is very cool,i played a bit as well back in the day,not for long tho";
-	cout << "\n\nIf you could travel somewhere,where would you like to go,"<< name<< "?: ";//we asking where the player likes to travel
+	cout << "\n\nIf you could travel somewhere,where would you like to go," << name << "?: ";//we asking where the player likes to travel
 	cout << "\n\n1.Europe\n2.Africa\n3.Asia\n4.North America\n5.South America\n6.Oceania: ";//the pool between what the player can choose
 	bool validchoice = false;
 	while (validchoice == false) {
@@ -73,27 +73,35 @@ int main() {
 		switch (choice) {
 		case '1':
 			system("cls");
-			cout << "Excellent choice!\nEurope is amazing!";
-			validchoice = true; break;// first choice
+			int Europe;
+			Europe = 100;
+			cout << "Excellent choice!\nEurope is amazing!\n\n";
+			cout << "So,where in Europe whould you like to go?\n";
+			cout << "1.Portugal\n2.Greece\n3.Hungary\n4.Netherlands\n5.Australia: ";
 		case'2':
 			system("cls");
-			cout << "Excellent choice!\nAfrica is amazing!";
+			cout << "Excellent choice!\nAfrica is amazing!\n\n";
+			cout << "1.Nigeria\n2.Egypt\n3.Zimbambue\n4.Cameroon\n5.Canada: ";
 			validchoice = true; break;//second choice
 		case'3':
 			system("cls");
-			cout << "Excellent choice!\nAsia is amazing!";
+			cout << "Excellent choice!\nAsia is amazing!\n\n";
+			cout << "1.Japan\n2.South Korea\n3.China\n4.Nepal\n5.Argentina: ";
 			validchoice = true; break;//third choice
 		case'4':
 			system("cls");
-			cout << "Excellent choice!\nNorth America is amazing!";
+			cout << "Excellent choice!\nNorth America is amazing!\n\n";
+			cout << "1.USA\n2.Canada\n3.Mexico\n4.Cuba\n5.Cyprus: ";
 			validchoice = true; break;//fourth choice
 		case'5':
 			system("cls");
-			cout << "Excellent choice!\nSouth America is amazing!";
+			cout << "Excellent choice!\nSouth America is amazing!\n\n";
+			cout << "1.Bolivia\n2.Argentina\n3.Brasil\n4.Guiana\n5.Spain: ";
 			validchoice = true; break;//fifth choice
 		case'6':
 			system("cls");
-			cout << "Excellent choice!\nOceania is amazing!";
+			cout << "Excellent choice!\nOceania is amazing!\n\n";
+			cout << "1.Australia\n2.New Ziland\n3.Indonesia\n4.Papua New Guinea\n5.Spain: ";
 			validchoice = true; break;//sixth choice
 		default:
 			system("cls");
@@ -103,6 +111,4 @@ int main() {
 	}
 	return 0;
 }
-
-
 
